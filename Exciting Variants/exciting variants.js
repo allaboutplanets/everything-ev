3e3e3a3b281f4898f7b286edcb39d64a239284e0
allaboutplanets <78298766+allaboutplanets@users.noreply.github.com>
2021-02-27 14:27:33 +0530

    master branch

---
+++ Exciting Variants/exciting variants.js
@@ -0,0 +1,135 @@
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+var keys = [
+  "Porsche Taycan",
+  "2021 Ford Mustang Mach-E",
+  "Tesla Roadster",
+  "2021 Audi E-Tron",
+  "2020 Jaguar I-Pace",
+  "BMW iX SUV",
+  "2021 Tesla Model S",
+  "2020 Tesla Model X",
+  "2021 Tesla Model Y"
+  "2021 Tesla Model 3",
+];
+var slider = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>";
+    }
+  }
+});
+
+slider.on("slideChange", function() {
+  console.log("SLIDE CHANGED");
+  gsap.to(".slide-text span", 0.2, {
+    x: "-100px"
+  });
+  gsap.to(".slide-number span", 0.2, {
+    x: "-100px"
+  });
+  gsap.to(".slide-detail span", 0.5, {
+    x: "-1000px"
+  });
+  gsap.to(".slide-detail-facts div", 0.5, {
+    x: "-1000px"
+  });
+  gsap.to(".swiper-slide-active", 0.5, {
+    scale: 0.85
+  });
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 20
+  });
+});
+
+slider.on("slideChangeTransitionEnd", function() {
+  gsap.to(".swiper-slide .slide-img", 1, {
+    rotation: 10
+  });
+  gsap.to(".slide-text span", 0.2, {
+    x: 0,
+    delay: 0.1
+  });
+  gsap.to(".slide-text span", 0, {
+    x: "100px"
+  });
+
+  gsap.to(".slide-number span", 0.2, {
+    x: 0
+  });
+  gsap.to(".slide-number span", 0, {
+    x: "100px"
+  });
+
+  gsap.to(".slide-detail span", 0.2, {
+    x: 0
+  });
+
+  gsap.to(".slide-detail-facts div", 0.5, {
+    x: 0
+  });
+
+  gsap.to(".swiper-slide-active", 0.5, {
+    scale: 1,
+    ease: Power4.easeOut
+  });
+
+  gsap.to(".swiper-slide-active .slide-text", 0, {
+    autoAlpha: 1
+  });
+  gsap.to(".swiper-slide-active .slide-number", 0, {
+    autoAlpha: 1
+  });
+
+  gsap.to(".swiper-slide-next .slide-text", 0, {
+    autoAlpha: 0
+  });
+  gsap.to(".swiper-slide-prev .slide-text", 0, {
+    autoAlpha: 0
+  });
+
+  gsap.to(".swiper-slide-next .slide-number", 0, {
+    autoAlpha: 0
+  });
+  gsap.to(".swiper-slide-prev .slide-number", 0, {
+    autoAlpha: 0
+  });
+});
+
+gsap.to(".rockbg1", 2, {
+  y: 12,
+  repeat: -1,
+  yoyo: true,
+  delay: 0
+});
+
+gsap.to(".swiper-slide-next .slide-text", 0, {
+  autoAlpha: 0
+});
+gsap.to(".swiper-slide-prev .slide-text", 0, {
+  autoAlpha: 0
+});
+
+gsap.to(".swiper-slide-next .slide-number", 0, {
+  autoAlpha: 0
+});
+gsap.to(".swiper-slide-prev .slide-number", 0, {
+  autoAlpha: 0
+});
+
+gsap.to(".swiper-slide", 0, {
+  scale: 0.85
+});
+
+gsap.to(".swiper-slide-active", 0, {
+  scale: 1
+});
